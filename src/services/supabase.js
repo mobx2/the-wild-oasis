25463009a3668d06d25f0486a31501b6a63a18fd
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-08-30 01:13:56 +0300

    connecting supabase with react

---
+++ src/services/supabase.js
@@ -0,0 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://nszkrgkrgqbrcnpvmdmd.supabase.co";
+
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5zemtyZ2tyZ3FicmNucHZtZG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0OTg0MDksImV4cCI6MjA3MjA3NDQwOX0.SCVXlNN73gE3OMeOVfZNoexRRVPIIYpqqbIPYDCkWoA";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
