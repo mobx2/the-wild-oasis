f3aaff4aede8c85b865c66708bd421d4e5f10524
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-08-29 23:18:29 +0300

    Building the app layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
   return (
@@ -17,15 +18,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="account" element={<Account />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="users" element={<Users />} />
+          </Route>
+
           <Route path="Login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="users" element={<Users />} />
         </Routes>
       </BrowserRouter>
     </>
