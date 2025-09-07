f14e1865b6518edb9b48265a9fe5c2f289d8cc5b
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-09-07 09:13:42 +0300

    Checking Out a Booking

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -1,6 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
+import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
+
 import BookingDataBox from "./BookingDataBox";
 import Row from "../../ui/Row";
 import Heading from "../../ui/Heading";
@@ -10,9 +15,6 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 import Spinner from "../../ui/Spinner";
 
-import { useMoveBack } from "../../hooks/useMoveBack";
-import { useBooking } from "./useBooking";
-
 const HeadingGroup = styled.div`
   display: flex;
   gap: 2.4rem;
@@ -21,9 +23,8 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
-
+  const { checkout, isCheckingOut } = useCheckout();
   const moveBack = useMoveBack();
-
   const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
@@ -52,6 +53,16 @@ function BookingDetail() {
         {status === "unconfirmed" && (
           <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
             Check in
+          </Button>
+        )}
+
+        {status === "checked-in" && (
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
           </Button>
         )}
 
