df7c827fd5c158371c49983dec7a520dc98ad187
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-09-13 06:51:09 +0300

    setting up header, headerMenu

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import Logout from "../features/authentication/Logout";
+import HeaderMenu from "./HeaderMenu";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -10,7 +10,7 @@ const StyledHeader = styled.header`
 function Header() {
   return (
     <StyledHeader>
-      <Logout />
+      <HeaderMenu />
     </StyledHeader>
   );
 }
