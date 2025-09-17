4426ae5920ff0996a68829cabfcaa9b619ca062c
Ibraheem Shaheen <he.mo234@yahoo.com>
2025-09-17 04:01:49 +0300

    Displaying stays for current day

--- src/features/check-in-out/CheckoutButton.jsx
+++ src/features/check-in-out/CheckoutButton.jsx
@@ -1,9 +1,19 @@
 import Button from "../../ui/Button";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+import { useCheckout } from "./useCheckout";
 
 function CheckoutButton({ bookingId }) {
+  const { checkout, isCheckingOut } = useCheckout();
+
   return (
-    <Button variation="primary" size="small">
-      Check out
+    <Button
+      variation="primary"
+      size="small"
+      onClick={() => checkout(bookingId)}
+      disabled={isCheckingOut}
+    >
+      {!isCheckingOut ? "Check out" : <SpinnerMini />}{" "}
     </Button>
   );
 }
--- src/features/check-in-out/TodayItem.jsx
+++ src/features/check-in-out/TodayItem.jsx
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+import { Flag } from "../../ui/Flag";
+import Tag from "../../ui/Tag";
+import Button from "../../ui/Button";
+import { Link } from "react-router-dom";
+
+import CheckoutButton from "./CheckoutButton";
+
 const StyledTodayItem = styled.li`
   display: grid;
   grid-template-columns: 9rem 2rem 1fr 7rem 9rem;
@@ -18,3 +25,35 @@ const StyledTodayItem = styled.li`
 const Guest = styled.div`
   font-weight: 500;
 `;
+
+function TodayItem({ activity }) {
+  const { id, status, guests, numNights } = activity;
+
+  return (
+    <StyledTodayItem>
+      {status === "unconfirmed" && <Tag type="green">Arriving</Tag>}
+      {status === "checked-in" && <Tag type="blue">Staying</Tag>}
+      {status === "checked-out" && <Tag type="grey">Departed</Tag>}
+
+      <Flag src={guests.countryFlag} alt={`Flag of ${guests.countryFlag}`} />
+
+      <Guest>{guests.fullName}</Guest>
+      <div>{numNights} nights</div>
+
+      {status === "unconfirmed" && (
+        <Button
+          size="small"
+          variation="primary"
+          as={Link}
+          to={`/checkin/${id}`}
+        >
+          Check in
+        </Button>
+      )}
+
+      {status === "checked-in" && <CheckoutButton bookingId={id} />}
+    </StyledTodayItem>
+  );
+}
+
+export default TodayItem;
